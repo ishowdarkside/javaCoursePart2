de1c16ba1c39d611015f7032cbff17bd30f5f8b1
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-09-03 22:22:11 +0200

    Finishedw Linkedlist Challenge

--- src/LinkedList/Main2.java
+++ src/LinkedList/Main2.java
@@ -2,11 +2,13 @@ package LinkedList;
 
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place(String name,int distance){
 
 @Override
 public String toString(){
+    //Override toString methodu Recorda kako bi prikazao samo ime i distancu lokacije
     return  String.format("%s (%d)",name,distance);
 }
 
@@ -14,6 +16,7 @@ public String toString(){
 
 public class Main2 {
     public static void main(String[] args) {
+        //Initiate linkedlist
         LinkedList<Place> placesToVisit = new LinkedList<>();
         Place adelaide = new Place("Adelaide",1374);
         addPlace(placesToVisit,adelaide);
@@ -24,15 +27,57 @@ public class Main2 {
         addPlace(placesToVisit,new Place("Darwin",3972));
         addPlace(placesToVisit,new Place("Melbourne",877));
         placesToVisit.addFirst(new Place("Sydney",0));
-        System.out.println(placesToVisit);
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        printMenu();
+
+        while(!quitLoop) {
+            if(!iterator.hasPrevious()) {
+                System.out.println("Originating : " + iterator.next());
+                forward = true;
+            }
+
+            if(!iterator.hasNext()) {
+                System.out.println("Final : " + placesToVisit.getLast());
+                forward = false;
+            }
+         System.out.println("Enter Value: ");
+         String menuItem = scanner.nextLine().toUpperCase().substring(0,1);
+         switch(menuItem){
+             case "F" -> {
+                 if(!forward) { // Reversing direction
+                     forward = true;
+                     if(iterator.hasNext()) iterator.next(); // Adjust position forward
+                 }
+                 if(iterator.hasNext()) System.out.println(iterator.next());
+             }
+             case "B" -> {
+                 if(forward){
+                     forward = false;
+                     if(iterator.hasPrevious()) iterator.previous();
+                 }
+                 if(iterator.hasPrevious()) System.out.println(iterator.previous());
+             }
+             case "M" -> printMenu();
+             case "L" -> System.out.println(placesToVisit);
+             default -> quitLoop = true;
+         }
+        }
     }
 
+    //private funckija koja prima 2 argumenta, LinkedListu i place
     private static void addPlace(LinkedList<Place> list,Place place){
+        //Ako lista sadrzi place uradi early return;
         if(list.contains(place)){
             System.out.println("Found duplicate: " + place);
             return;
         }
 
+        //Za svaki item iz list provjeri da li je ime == Place-u koji pokusavamo da unesemo, ako jeste, uradi early return;
         for (Place p : list){
             if(p.name().equalsIgnoreCase(place.name())){
                 System.out.println("Found duplicate: " + place);
@@ -40,6 +85,8 @@ public class Main2 {
             }
         }
 
+        //Initiate matchedIndex var koji pocinje od 0 (linkedlist 0 indexed logy). Za svaki place u ovoj listi provjeri da li je distance veca od tog current elementa
+        //ako jeste, stavi ga na taj current index i povecavaj index kako loopas kroz listu..Na taj nacin, places ce biti ascending sortirani u zavisnosti od distance.
         int matchedIndex = 0;
         for(var listPlace : list){
             if(place.distance() < listPlace.distance()){
@@ -49,5 +96,15 @@ public class Main2 {
             matchedIndex++;
         }
         list.add(place);
+    }
+
+    private static void printMenu(){
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackwards
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
     }
 }
